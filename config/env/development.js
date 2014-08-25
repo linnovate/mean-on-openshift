6ba7119bb747d5504573831b5ebe4a445bb7f0ed
Shai Weinstein <shai.weinstein@gmail.com>
2014-08-25 13:39:36 +0300

    moving config

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-   db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME,
+  db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME,
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
@@ -9,36 +9,61 @@ module.exports = {
    * and http://mongoosejs.com/docs/connections.html for more information
    */
   http: {
-        port: process.env.OPENSHIFT_NODEJS_PORT,
+	port: process.env.OPENSHIFT_NODEJS_PORT,
   },
   hostname: process.env.OPENSHIFT_NODEJS_IP,
-    app: {
-        name: 'MEAN - FullStack JS - OpenShift'
+  dbOptions: {
+    /*
+    server: {
+        socketOptions: {
+            keepAlive: 1
+        },
+        poolSize: 5
     },
-    facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    replset: {
+      rs_name: 'myReplicaSet',
+      poolSize: 5
     },
-    twitter: {
-        clientID: 'CONSUMER_KEY',
-        clientSecret: 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    },
-    github: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-    google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
-    linkedin: {
-        clientID: 'API_KEY',
-        clientSecret: 'SECRET_KEY',
-        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    db: {
+      w: 1,
+      numberOfRetries: 2
+    }
+    */
+  },
+  app: {
+    name: 'MEAN - A Modern Stack - OpenShift'
+  },
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  github: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  linkedin: {
+    clientID: 'API_KEY',
+    clientSecret: 'SECRET_KEY',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER',
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
     }
   }
 };
