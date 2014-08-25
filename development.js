fc4773847809dc826dd2609a69ee4e1d41619d74
Shai Weinstein <shai.weinstein@gmail.com>
2014-08-25 13:34:07 +0300

    fix for config

---
+++ development.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME,
+  /**
+   * Database options that will be passed directly to mongoose.connect
+   * Below are some examples.
+   * See http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connect-options
+   * and http://mongoosejs.com/docs/connections.html for more information
+   */
+  http: {
+	port: process.env.OPENSHIFT_NODEJS_PORT,
+  },
+  hostname: process.env.OPENSHIFT_NODEJS_IP,
+  dbOptions: {
+    /*
+    server: {
+        socketOptions: {
+            keepAlive: 1
+        },
+        poolSize: 5
+    },
+    replset: {
+      rs_name: 'myReplicaSet',
+      poolSize: 5
+    },
+    db: {
+      w: 1,
+      numberOfRetries: 2
+    }
+    */
+  },
+  app: {
+    name: 'MEAN - A Modern Stack - OpenShift'
+  },
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  github: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  linkedin: {
+    clientID: 'API_KEY',
+    clientSecret: 'SECRET_KEY',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER',
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
+    }
+  }
+};
