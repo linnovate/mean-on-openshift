6928f5b0f6879467229fd6bfc43c16c0cc878e66
pgudhe <pgudhe@etouch.net>
2016-06-16 22:22:32 +0530

    fixing linkedin Oauth

--- packages/access/server/config/passport.js
+++ packages/access/server/config/passport.js
@@ -194,6 +194,7 @@ module.exports = function(passport) {
           email: profile.emails[0].value,
           username: profile.emails[0].value,
           provider: 'linkedin',
+		  linkedin: profile._json,
           roles: ['authenticated']
         });
         user.save(function(err) {
